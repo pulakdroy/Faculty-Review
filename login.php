1a095cf4c0b94d2dd13b6bbd4f7c83e6304f524a
arundhati1124 <arundhatisarker24@gmail.com>
2024-01-11 10:20:32 +0600

    arun_index,login

--- login.php
+++ login.php
@@ -3,28 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log in</title>
+    <title>Login</title>
     <link rel="stylesheet" href="login.css">
 </head>
 <body>
-    <?php
-        if (isset($_POST["login"])) {
-           $user_name = $_POST["username"];
-           $password = $_POST["password"];
-            require_once "database1.php";
-            $sql = "SELECT * FROM account WHERE username = '$user_name'";
-        }
-        ?>
     <div class="login">
         <h1>Faculty Review</h1>
-        <form method="post" action="server_login">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required>
-            
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
 
-            <button type="submit">Login</button>
+
+    <?php
+        if (isset($_POST["login"])) {
+           $Username = $_POST["username"];
+           $Password = $_POST["password"];
+            require_once "database1.php";
+            $sql = "SELECT * FROM account WHERE Username = '$Username'";
+            $result=mysqli_query($conn, $sql);
+            $user=mysqli_fetch_array($result, MYSQLI_ASSOC);//to access to the column of database
+            if ($user){
+                if (password_verify($Password, $user["Password"])){
+                    header("Location: index.php");//redirect to home page
+                    die();
+                }else{
+                    echo"<div class= 'alert alert-danger'> password dose not match</div>";
+                }
+            }else{
+                echo"<div class= 'alert alert-danger'> username dose not match</div>";
+            }
+        }
+        ?>
+        
+        <form action="login.php" method="post" >
+            <div class="form-group">
+                <input type="text" class="form-control" name="username" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <input type="password" class="form-control" name="password" class="form-control">
+            </div>   
+            <div class="form-btn">
+                <input type="submit" value="Login" name="login" class="btn btn-primary">
+
+            </div>     
         </form>
 
         <p>Forget password? <a href="">Forget password</a></p>
